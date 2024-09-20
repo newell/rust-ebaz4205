c36a9797edf4d7a93ddffd642f481cfb654dc29f
newell <newell.jensen@gmail.com>
2024-09-20 16:41:47 -0700

    Update firmware to test PHY

--- src/firmware/src/main.rs
+++ src/firmware/src/main.rs
@@ -1,43 +1,53 @@
 #![no_std]
 #![no_main]
 
-use log::info;
+extern crate alloc;
+
+use log::{info, warn};
+use alloc::{vec, format};
 
 use libcortex_a9::asm;
 use libboard_zynq::{
+    eth::Eth,
+    smoltcp::{
+        self,
+        iface::{EthernetInterfaceBuilder, NeighborCache},
+        time::Instant,
+        wire::IpCidr,
+    },
     timer::GlobalTimer,
-    logger
+    logger,
+    slcr
 };
+use libconfig::{net_settings, Config};
 use libsupport_zynq::ram;
-
-pub mod led;
-use led::LED;
+use libregister::RegisterW;
 
 // #[path = "../../../build/pl.rs"]
 // mod pl;
 
-// fn init_gateware() {
-//     // Set up PS->PL clocks
-//     slcr::RegisterBlock::unlocked(|slcr| {
-//         // As we are touching the mux, the clock may glitch, so reset the PL.
-//         slcr.fpga_rst_ctrl.write(
-//             slcr::FpgaRstCtrl::zeroed()
-//                 .fpga0_out_rst(true)
-//                 .fpga1_out_rst(true)
-//                 .fpga2_out_rst(true)
-//                 .fpga3_out_rst(true)
-//         );
-//         slcr.fpga0_clk_ctrl.write(
-//             slcr::Fpga0ClkCtrl::zeroed()
-//                 .src_sel(slcr::PllSource::IoPll)
-//                 .divisor0(8)
-//                 .divisor1(1)
-//         );
-//         slcr.fpga_rst_ctrl.write(
-//             slcr::FpgaRstCtrl::zeroed()
-//         );
-//     });
-// }
+fn init_gateware() {
+    // Set up PS->PL clocks
+    slcr::RegisterBlock::unlocked(|slcr| {
+        // As we are touching the mux, the clock may glitch, so reset the PL.
+        slcr.fpga_rst_ctrl.write(
+            slcr::FpgaRstCtrl::zeroed()
+                .fpga0_out_rst(true)
+                .fpga1_out_rst(true)
+                .fpga2_out_rst(true)
+                .fpga3_out_rst(true)
+        );
+        slcr.fpga0_clk_ctrl.write(
+            slcr::Fpga0ClkCtrl::zeroed()
+                .src_sel(slcr::PllSource::IoPll)
+                .divisor0(8)
+                .divisor1(1)
+        );
+        slcr.fpga_rst_ctrl.write(
+            slcr::FpgaRstCtrl::zeroed()
+        );
+    });
+}
 
 // fn identifier_read(buf: &mut [u8]) -> &str {
 //     unsafe {
@@ -52,38 +62,107 @@ use led::LED;
 //     }
 // }
 
+#[derive(PartialEq)]
+enum State {
+    Idle,
+    SendingMessage
+}
+
 #[no_mangle]
 pub fn main_core0() {
     GlobalTimer::start();
     logger::init().unwrap();
     log::set_max_level(log::LevelFilter::Info);
 
-    info!("Rust EBAZ4205 firmware starting...");
+    info!("EBAZ4205 firmware starting...");
 
     ram::init_alloc_core0();
 
-    // init_gateware();
+    init_gateware();
     // info!("detected gateware: {}", identifier_read(&mut [0; 64]));
 
+    let cfg = match Config::new() {
+        Ok(cfg) => cfg,
+        Err(err) => {
+            warn!("config initialization failed: {}", err);
+            Config::new_dummy()
+        }
+    };
+
+    let net_addresses = net_settings::get_addresses(&cfg);
+    log::info!("Network addresses: {}", net_addresses);
+    let eth = Eth::eth0(net_addresses.hardware_addr.0.clone());
+    let eth = eth.start_rx(8);
+    let mut eth = eth.start_tx(8);
+
+    let mut neighbor_map = [None; 2];
+    let neighbor_cache = NeighborCache::new(&mut neighbor_map[..]);
+    let mut ip_addrs = [IpCidr::new(net_addresses.ipv4_addr, 0)];
+    let mut interface = EthernetInterfaceBuilder::new(&mut eth)
+        .ethernet_addr(net_addresses.hardware_addr)
+        .ip_addrs(&mut ip_addrs[..])
+        .neighbor_cache(neighbor_cache)
+        .finalize();
+
+    let mut rx_storage = vec![0; 64];
+    let mut tx_storage = vec![0; 4096];
+
+    let mut socket_set_entries: [_; 1] = Default::default();
+    let mut sockets = smoltcp::socket::SocketSet::new(&mut socket_set_entries[..]);
+
+    let tcp_rx_buffer = smoltcp::socket::TcpSocketBuffer::new(&mut rx_storage[..]);
+    let tcp_tx_buffer = smoltcp::socket::TcpSocketBuffer::new(&mut tx_storage[..]);
+    let tcp_socket = smoltcp::socket::TcpSocket::new(tcp_rx_buffer, tcp_tx_buffer);
+    let tcp_handle = sockets.add(tcp_socket);
+
     let timer = unsafe { GlobalTimer::get() };
+    let mut state = State::Idle;
 
-    let mut led = LED::new();
-
-    log::info!("Start blinking LEDs...");
-    let mut led_state = false;
-
+    log::info!("Waiting for connections...");
     loop {
-        let current_time = timer.get_time().0;
+        let timestamp = Instant::from_millis(timer.get_time().0 as i64);
+        let message = format!("Hello! Current timestamp: {:?}", timestamp);
 
-        log::info!("LED state toggled at time: {}", current_time);
+        log::info!("{}", message);
 
-        // Toggle LED state
-        led.toggle(led_state);
-        led_state = !led_state; // Invert state for the next toggle
+        {
+            let socket = &mut *sockets.get::<smoltcp::socket::TcpSocket>(tcp_handle);
 
+            if !socket.is_open() {
+                socket.listen(1550).unwrap();
+            }
 
-        for _ in 0..100_000_000 {
-            asm::nop();
+            if socket.may_recv() {
+                let start_cmd = socket.recv(|data| (data.len(), data.len() > 0)).unwrap();
+                if start_cmd && state == State::Idle {
+                    log::info!("Sending hello message");
+                    state = State::SendingMessage;
+                }
+            } else if socket.may_send() {
+                log::info!("disconnected");
+                state = State::Idle;
+                socket.close();
+            }
+
+            if state == State::SendingMessage {
+                match socket.send_slice(message.as_bytes()) {
+                    Ok(_) => {
+                        log::info!("Message sent: {}", message);
+                        state = State::Idle;
+                    }
+                    Err(e) => {
+                        log::error!("Error while transmitting: {}", e);
+                        state = State::Idle;
+                        socket.close();
+                    }
+                }
+            }
+        }
+
+        match interface.poll(&mut sockets, timestamp) {
+            Ok(_) => (),
+            Err(smoltcp::Error::Unrecognized) => (),
+            Err(err) => log::error!("Network error: {}", err),
         }
     }
 }
@@ -94,3 +173,62 @@ pub fn main_core1() {
         asm::wfe();
     }
 }
+
+// // BLINKY LED FIRMWARE
+// #![no_std]
+// #![no_main]
+
+// use log::info;
+
+// use libcortex_a9::asm;
+// use libboard_zynq::{
+//     timer::GlobalTimer,
+//     logger
+// };
+// use libsupport_zynq::ram;
+
+// pub mod led;
+// use led::LED;
+
+// #[no_mangle]
+// pub fn main_core0() {
+//     GlobalTimer::start();
+//     logger::init().unwrap();
+//     log::set_max_level(log::LevelFilter::Info);
+
+//     info!("Rust EBAZ4205 firmware starting...");
+
+//     ram::init_alloc_core0();
+
+//     // init_gateware();
+//     // info!("detected gateware: {}", identifier_read(&mut [0; 64]));
+
+//     let timer = unsafe { GlobalTimer::get() };
+
+//     let mut led = LED::new();
+
+//     log::info!("Start blinking LEDs...");
+//     let mut led_state = false;
+
+//     loop {
+//         let current_time = timer.get_time().0;
+
+//         log::info!("LED state toggled at time: {}", current_time);
+
+//         // Toggle LED state
+//         led.toggle(led_state);
+//         led_state = !led_state; // Invert state for the next toggle
+
+
+//         for _ in 0..100_000_000 {
+//             asm::nop();
+//         }
+//     }
+// }
+
+// #[no_mangle]
+// pub fn main_core1() {
+//     loop {
+//         asm::wfe();
+//     }
+// }
--- src/gateware/ebaz4205.py
+++ src/gateware/ebaz4205.py
@@ -2,24 +2,161 @@
 
 import argparse
 from operator import itemgetter
+from toolz.curried import *  # noqa
 
 from migen import *
-from migen.build.platforms import ebaz4205
+
+# from migen.build.platforms import ebaz4205
 from migen.build.generic_platform import Pins, Subsignal, IOStandard, Misc
 from migen_axi.integration.soc_core import SoCCore
 from misoc.interconnect.csr import *
 
-from artiq.gateware import rtio
-from artiq.gateware.rtio.phy import (
-    ttl_simple,
-    dds,  # Need to create module for AD9834
-)
-from artiq.gateware.rtio.xilinx_clocking import fix_serdes_timing_path
+# from migen.build.generic_platform import *
+from migen.build.xilinx import XilinxPlatform
 
-import dma
-import analyzer
 
-from config import write_csr_file, write_mem_file, write_rustc_cfg_file
+_io = [
+    # Green and Red LEDs
+    ("user_led", 0, Pins("W13"), IOStandard("LVCMOS33")),
+    ("user_led", 1, Pins("W14"), IOStandard("LVCMOS33")),
+    # Push Buttons
+    ("user_btn", 0, Pins("A17"), IOStandard("LVCMOS33")),
+    ("user_btn", 1, Pins("A14"), IOStandard("LVCMOS33")),
+    # UART
+    (
+        "serial",
+        0,
+        Subsignal("tx", Pins("A16")),
+        Subsignal("rx", Pins("F15")),
+        IOStandard("LVCMOS33"),
+    ),
+    # SD Card
+    (
+        "sdcard",
+        0,
+        Subsignal("detect", Pins("A12"), Misc("PULLUP True")),
+        Subsignal("data", Pins("E12 A9 F13 B15")),
+        Subsignal("cmd", Pins("C17")),
+        Subsignal("clk", Pins("D14")),
+        Subsignal("cd", Pins("B15")),
+        IOStandard("LVCMOS33"),
+        Misc("SLEW=FAST"),
+    ),
+    # NAND Flash
+    (
+        "nandflash",
+        0,
+        Subsignal("nand_data", Pins("A6 A5 B7 E8 B5 E9 C6 D9")),
+        Subsignal("nand_ce", Pins("E6")),
+        Subsignal("nand_re", Pins("D5")),
+        Subsignal("nand_we", Pins("D6")),
+        Subsignal("nand_ale", Pins("B8")),
+        Subsignal("nand_cle", Pins("D8")),
+        Subsignal("nand_rb", Pins("C5")),
+        IOStandard("LVCMOS33"),
+    ),
+    # ETH PHY
+    (
+        "gmii",
+        0,
+        Subsignal("rxd", Pins("Y16 V16 V17 Y17")),
+        Subsignal("txd", Pins("W18 Y18 V18 Y19")),
+        Subsignal("rx_clk", Pins("U14")),
+        Subsignal("tx_clk", Pins("U15")),
+        Subsignal("rx_dv", Pins("W16")),
+        Subsignal("tx_en", Pins("W19")),
+        IOStandard("LVCMOS33"),
+    ),
+    (
+        "mdio",
+        0,
+        Subsignal("mdio", Pins("Y14")),
+        Subsignal("mdc", Pins("W15")),
+        IOStandard("LVCMOS33"),
+    ),
+]
+
+
+# DATA1-3 2x10 2.0mm Pitch
+# J3 and J5 1x4 2.54mm Pitch
+_connectors = [
+    (
+        "DATA1",
+        {
+            "DATA1-5": "A20",
+            "DATA1-6": "H16",
+            "DATA1-7": "B19",
+            "DATA1-8": "B20",
+            "DATA1-9": "C20",
+            "DATA1-11": "H17",
+            "DATA1-13": "D20",
+            "DATA1-14": "D18",
+            "DATA1-15": "H18",
+            "DATA1-16": "D19",
+            "DATA1-17": "F20",
+            "DATA1-18": "E19",
+            "DATA1-19": "F19",
+            "DATA1-20": "K17",
+        },
+    ),
+    (
+        "DATA2",
+        {
+            "DATA2-5": "G20",
+            "DATA2-6": "J18",
+            "DATA2-7": "G19",
+            "DATA2-8": "H20",
+            "DATA2-9": "J19",
+            "DATA2-11": "K18",
+            "DATA2-13": "K19",
+            "DATA2-14": "J20",
+            "DATA2-15": "L16",
+            "DATA2-16": "L19",
+            "DATA2-17": "M18",
+            "DATA2-18": "L20",
+            "DATA2-19": "M20",
+            "DATA2-20": "L17",
+        },
+    ),
+    (
+        "DATA3",
+        {
+            "DATA3-5": "M19",
+            "DATA3-6": "N20",
+            "DATA3-7": "P18",
+            "DATA3-8": "M17",
+            "DATA3-9": "N17",
+            "DATA3-11": "P20",
+            "DATA3-13": "R18",
+            "DATA3-14": "R19",
+            "DATA3-15": "P19",
+            "DATA3-16": "T20",
+            "DATA3-17": "U20",
+            "DATA3-18": "T19",
+            "DATA3-19": "V20",
+            "DATA3-20": "U19",
+        },
+    ),
+    (
+        "J3",
+        {
+            "J3-4-TX": "U12",
+            "J3-3-RX": "V13",
+        },
+        "J5",
+        {
+            "J5-4-TX": "V12",
+            "J5-3-RX": "V15",
+        },
+    ),
+]
+
+
+class Platform(XilinxPlatform):
+    def __init__(self):
+        XilinxPlatform.__init__(
+            self, "xc7z010-clg400-1", _io, _connectors, toolchain="vivado"
+        )
 
 
 __all__ = ["Platform"]
@@ -84,7 +221,8 @@ class EBAZ4205(SoCCore):
     def __init__(self, acpki=False):
         self.acpki = acpki
 
-        platform = ebaz4205.Platform()
+        # platform = ebaz4205.Platform()
+        platform = Platform()
         platform.toolchain.bitstream_commands.extend(
             [
                 "set_property BITSTREAM.GENERAL.COMPRESS True [current_design]",
@@ -93,101 +231,65 @@ class EBAZ4205(SoCCore):
         platform.add_extension(_ps)
         platform.add_extension(_ddr)
 
+        # FCLK is tied to pin U18
+        platform.add_extension(
+            [
+                ("fclk", 0, Pins("U18"), IOStandard("LVCMOS33")),
+            ]
+        )
+
         gmii = platform.request("gmii")
+
         platform.add_period_constraint(gmii.rx_clk, 10)
         platform.add_period_constraint(gmii.tx_clk, 10)
-
-        gmii_txd = Signal(8)
-        gmii_rxd = Signal(8)
-        gmii_tx_er = Signal()
-        gmii_rx_er = Signal()
-        gmii_crs = Signal()
-        gmii_col = Signal()
-
-        self.comb += [
-            gmii_tx_er.eq(0),
-            gmii_rx_er.eq(0),
-            gmii_crs.eq(0),
-            gmii_col.eq(0),
-            gmii_txd.eq(0),
-            gmii_rxd.eq(0),
-            gmii_txd.eq(Cat(gmii.txd, Signal(4, reset=0, name="gmii_txd0"))),
-            gmii_rxd.eq(Cat(gmii.rxd, Signal(4, reset=0, name="gmii_rxd0"))),
-        ]
-        gmii_layout = gmii.layout[2:] + [
-            (sig.backtrace[-1][0].split("_", 1)[-1], sig.nbits)
-            for sig in [gmii_txd, gmii_rxd, gmii_tx_er, gmii_rx_er, gmii_crs, gmii_col]
-        ]
-        gmii_rec = Record(gmii_layout)
-        ident = self.__class__.__name__
-        if self.acpki:
-            ident = "acpki_" + ident
-        SoCCore.__init__(
-            self, platform=platform, csr_data_width=32, ident=ident, enet0=gmii_rec
+        platform.add_platform_command(
+            "set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets gmii_tx_clk_IBUF]"
         )
-        fix_serdes_timing_path(platform)
-        # When using pd_cd_sys, the default clock coming from fclk.clk[0] is 100 MHz
-        self.config["RTIO_FREQUENCY"] = "100"
+
+        ident = self.__class__.__name__
+        SoCCore.__init__(
+            self,
+            platform=platform,
+            csr_data_width=32,
+            ident=ident,
+        )
         platform.add_period_constraint(self.ps7.cd_sys.clk, 10)
 
-        mdio = platform.request("mdio")
-        mdio_i = Signal(name_override="i")
-        mdio_o = Signal(name_override="o")
-        mdio_t_n = Signal(name_override="t_n")
         self.comb += [
-            mdio_i.eq(self.ps7.enet0.enet.mdio.o),
-            self.ps7.enet0.enet.mdio.i.eq(mdio_o),
-            mdio_t_n.eq(self.ps7.enet0.enet.mdio.t_n),
-            self.ps7.enet0.enet.mdio.mdc.eq(mdio.mdc),
+            # Inputs
+            self.ps7.enet0.enet.gmii.tx_clk.eq(gmii.tx_clk),
+            self.ps7.enet0.enet.gmii.rx_clk.eq(gmii.rx_clk),
+            self.ps7.enet0.enet.gmii.rx_dv.eq(gmii.rx_dv),
+            self.ps7.enet0.enet.gmii.rxd.eq(gmii.rxd),
+            # Outputs
+            gmii.tx_en.eq(self.ps7.enet0.enet.gmii.tx_en),
+            gmii.txd.eq(self.ps7.enet0.enet.gmii.txd),
         ]
-        self.specials += [
-            Instance("IOBUF", i_I=mdio_i, io_IO=mdio.mdio, o_O=mdio_o, i_T=mdio_t_n)
+
+        # LEDs
+        user_led_0 = platform.request("user_led", 0)
+        user_led_1 = platform.request("user_led", 1)
+        self.comb += [
+            user_led_0.eq(self.ps7.gpio.o[0]),
+            user_led_1.eq(self.ps7.gpio.o[1]),
         ]
 
-        self.rtio_channels = []
-        for i in (0, 1):
-            print("USER LED at RTIO channel 0x{:06x}".format(len(self.rtio_channels)))
-            user_led = self.platform.request("user_led", i)
-            phy = ttl_simple.Output(user_led)
-            self.submodules += phy
-            self.rtio_channels.append(rtio.Channel.from_phy(phy))
-        self.config["RTIO_LOG_CHANNEL"] = len(self.rtio_channels)
-        self.rtio_channels.append(rtio.LogChannel())
+        # FCLK
+        fclk = platform.request("fclk")
+        self.comb += fclk.eq(self.ps7.fclk.clk[0])
 
-        self.submodules.rtio_tsc = rtio.TSC(glbl_fine_ts_width=3)
-        self.submodules.rtio_core = rtio.Core(self.rtio_tsc, self.rtio_channels)
-        self.csr_devices.append("rtio_core")
-        if self.acpki:
-            import acpki
-
-            self.config["KI_IMPL"] = "acp"
-            self.submodules.rtio = acpki.KernelInitiator(
-                self.rtio_tsc,
-                bus=self.ps7.s_axi_acp,
-                user=self.ps7.s_axi_acp_user,
-                evento=self.ps7.event.o,
-            )
-            self.csr_devices.append("rtio")
-        else:
-            self.config["KI_IMPL"] = "csr"
-            self.submodules.rtio = rtio.KernelInitiator(self.rtio_tsc, now64=True)
-            self.csr_devices.append("rtio")
-
-        self.submodules.rtio_dma = dma.DMA(self.ps7.s_axi_hp0)
-        self.csr_devices.append("rtio_dma")
-
-        self.submodules.cri_con = rtio.CRIInterconnectShared(
-            [self.rtio.cri, self.rtio_dma.cri], [self.rtio_core.cri]
+        # MDIO/MDC
+        mdio = platform.request("mdio")
+        self.comb += [
+            mdio.mdc.eq(self.ps7.enet0.enet.mdio.mdc),
+        ]
+        self.specials += Instance(
+            "IOBUF",
+            i_I=self.ps7.enet0.enet.mdio.o,
+            io_IO=mdio.mdio,
+            o_O=self.ps7.enet0.enet.mdio.i,
+            i_T=self.ps7.enet0.enet.mdio.t_n,
         )
-        self.csr_devices.append("cri_con")
-
-        self.submodules.rtio_moninj = rtio.MonInj(self.rtio_channels)
-        self.csr_devices.append("rtio_moninj")
-
-        self.submodules.rtio_analyzer = analyzer.Analyzer(
-            self.rtio_tsc, self.rtio_core.cri, self.ps7.s_axi_hp1
-        )
-        self.csr_devices.append("rtio_analyzer")
 
 
 def main():
@@ -213,15 +315,15 @@ def main():
     )
     args = parser.parse_args()
 
-    soc = EBAZ4205(acpki=args.acpki)
+    soc = EBAZ4205()  # acpki=args.acpki)
     soc.finalize()
 
-    if args.r is not None:
-        write_csr_file(soc, args.r)
-    if args.m is not None:
-        write_mem_file(soc, args.m)
-    if args.c is not None:
-        write_rustc_cfg_file(soc, args.c)
+    # if args.r is not None:
+    #     write_csr_file(soc, args.r)
+    # if args.m is not None:
+    #     write_mem_file(soc, args.m)
+    # if args.c is not None:
+    #     write_rustc_cfg_file(soc, args.c)
     if args.g is not None:
         soc.build(build_dir=args.g)
 
